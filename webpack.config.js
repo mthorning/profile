250826b411817eb9eeabdc60e924a5e9f4d77185
Matt Thorning <matthewthorning@gmail.com>
2020-01-31 20:28:03 +0000

    added css to webpack build and optimized

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,32 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  entry: "./lib/es6/src/Index.bs.js",
-  // If you ever want to use webpack during development, change 'production'
-  // to 'development' as per webpack documentation. Again, you don't have to
-  // use webpack or any other bundler during development! Recheck README if
-  // you didn't know this
+entry: ["./lib/es6/src/Index.bs.js", "./css/index.js"],
   mode: "production",
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "index.js",
-    chunkFilename: "[name].bundle[hash].js"
+    filename: "index-[hash].js",
+    chunkFilename: "[name]-[hash].js"
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "indexProduction.html" }),
-    new CopyPlugin([
-      { from: "css", to: "assets" },
-      { from: "netlify", to: "." }
-    ]),
-    new CleanWebpackPlugin()
+    new CopyPlugin([{ from: "netlify", to: "." }, { from: "images", to: "images"}]),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+        filename: '[name]-[hash].css'
+    })
   ],
   module: {
     rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
       {
         test: /\.(png|jpg|gif)$/i,
         use: [
@@ -39,6 +42,8 @@ module.exports = {
     ]
   },
   optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()],
     splitChunks: {
       chunks: "all"
     }
